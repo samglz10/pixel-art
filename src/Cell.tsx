9eabf0361dbe913039f00a5e1469285b3e60eb10
Samuel Gonzalez <samglz10@gmail.com>
2024-04-06 13:02:08 -0400

    cell css active class

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 function Cell(){
-    
+    const [active, setActive] = useState(false);
+
+    function handleCellChange(){
+        setActive(true);
+    }
+
+
     return(
-
-    <div className="cell">+</div>
-
+    <div 
+        style={ active ? {background:"red"} : undefined}
+        className="cell" 
+        onClick={handleCellChange}
+    ></div>
     )
 }
 export default Cell;
